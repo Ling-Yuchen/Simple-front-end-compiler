d4f8d8526c6b5a5d23750243aac384695346c0f7
201250007 <201250007@smail.nju.edu.cn>
2021-10-07 00:07:18 +0800

    定义了Tag类中的常量

--- Compiler/src/lexer/Lexer.java
+++ Compiler/src/lexer/Lexer.java
@@ -1,4 +1,9 @@
 package lexer;
 
+import java.util.Hashtable;
+
 public class Lexer {
+    public static int line = 1;
+    public static final char peek = ' ';
+    Hashtable words = new Hashtable();
 }
--- Compiler/src/lexer/Tag.java
+++ Compiler/src/lexer/Tag.java
@@ -1,4 +1,16 @@
 package lexer;
 
+/**
+ * Tag类 定义了各词法单元对应的常量
+ * ASCII字符通常被转化为 0~255 之间的整数
+ * 故用大于 255 的整数来表示终结符号
+*/
+
 public class Tag {
+    public static final int
+            AND = 256,      BASIC = 257,    BREAK = 258,    DO = 259,
+            ELSE = 260,     EQ = 261,       FALSE = 262,    GE = 263,
+            ID = 264,       IF = 265,       INDEX = 266,    LE = 267,
+            MINUS = 268,    NE = 269,       NUM = 270,      OR = 271,
+            REAL = 272,     TEMP = 273,     TRUE = 274,     WHILE = 275;
 }
